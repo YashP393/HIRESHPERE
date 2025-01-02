71815dfd97b023c12c30a019bdcd8947a301932b
Ayan Mansuri (Ghost_.) <mansuriayan243@gmail.com>
2025-01-02 12:18:22 +0530

    Update manage.py

--- manage.py
+++ manage.py
@@ -2,7 +2,8 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-
+# dhfhdsoighwriohgrihgfkxanhrihriekgnowrkgoirejgrwigrwijgwkogjreiohrwowign
+# girhgrjhorphjetpohjeophjgropgjrfjprwohjrwophjepohjerojh
 
 def main():
     """Run administrative tasks."""
